f09e009af990d65023ced74297f110c49555a750
Kharisma Maulana Pasaribu <kharismamaulana1@gmail.com>
2020-04-30 09:12:34 +0700

    First commit

--- notif.sql
+++ notif.sql
@@ -1,5 +1,5 @@
 /*
- Navicat Premium Data Transfer
+ Author Kharisma Maulana Pasaribu
 
  Source Server         : Localhost
  Source Server Type    : MySQL
@@ -16,7 +16,6 @@
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
 -- ----------------------------
 -- Table structure for comments
 -- ----------------------------
@@ -28,10 +27,4 @@ CREATE TABLE `comments`  (
   `comment_status` int(1) NOT NULL,
   PRIMARY KEY (`comment_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
-
--- ----------------------------
--- Records of comments
--- ----------------------------
-INSERT INTO `comments` VALUES (1, 'lana', 'lana', 1);
-
 SET FOREIGN_KEY_CHECKS = 1;
